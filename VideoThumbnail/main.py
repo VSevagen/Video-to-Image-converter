1618e45250b94fa5e416e1deecb4826b80b56842
sevagen <sevagenv@gmail.com>
2020-07-23 21:11:59 +0530

    Add prompt for pdf making

--- VideoThumbnail/main.py
+++ VideoThumbnail/main.py
@@ -7,3 +7,9 @@ exec(code)
 with open("removeDup.py", "rb") as source_file:
     code = compile(source_file.read(), "removeDup.py", "exec")
 exec(code)
+
+prompt = int(input("Make a pdf out of the images 0(No) 1(Yes) >>>"))
+if prompt == 1:
+    with open("pdfMaker.py", "rb") as source_file:
+        code = compile(source_file.read(), "pdfMaker.py", "exec")
+    exec(code)
