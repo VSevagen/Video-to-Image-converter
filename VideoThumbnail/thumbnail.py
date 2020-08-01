5f9af8844e6abb38c9b93662f32244f7503ec106
sevagen <sevagenv@gmail.com>
2020-08-01 14:31:50 +0530

    Fix frame rate

--- VideoThumbnail/thumbnail.py
+++ VideoThumbnail/thumbnail.py
@@ -2,15 +2,15 @@ import cv2
 import os
 
 path = '/home/sevagen/Desktop/Video-to-Image-converter/VideoThumbnail/images'
-vidcap = cv2.VideoCapture('videoplayback.mp4')
+vidcap = cv2.VideoCapture('CometChat at Techstars Boulder 2019 Demo Day.mp4')
 def getFrame(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     hasFrames,image = vidcap.read()
     if hasFrames:
-        cv2.imwrite(os.path.join(path,"image"+str(count)+".jpg"), image) 
+        cv2.imwrite(os.path.join(path,str(count)+".jpg"), image) 
     return hasFrames
 sec = 0
-frameRate = 0.5
+frameRate = 1
 count=1
 success = getFrame(sec)
 while success:
