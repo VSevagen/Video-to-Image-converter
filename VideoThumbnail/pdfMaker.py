cbfe5b955de04b349f5964d57c69abf50cbb6bae
sevagen <sevagenv@gmail.com>
2020-08-01 11:12:02 +0530

    Fix: Sort images before making pdf

--- VideoThumbnail/pdfMaker.py
+++ VideoThumbnail/pdfMaker.py
@@ -1,10 +1,15 @@
 import img2pdf
 import os
+import numpy as np
 
 path = os.getcwd()+"/images"
 os.chdir(path)
+myimages = []
+dirFiles = os.listdir(os.getcwd())
+fnames = sorted([fname for fname in os.listdir(os.getcwd()) if fname.endswith('.jpg')], key=lambda f: int(f.rsplit(os.path.extsep, 1)[0].rsplit(None,1)[-1]))
+
 with open("output.pdf", "wb") as f:
-    f.write(img2pdf.convert([i for i in os.listdir(os.getcwd()) if i.endswith(".jpg")]))
+    f.write(img2pdf.convert([i for i in fnames if i.endswith(".jpg")]))
 
 imgDir = os.listdir(path)
 for image in imgDir:
